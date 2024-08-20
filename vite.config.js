ab0e7be07278d5d038272f0d817681d41407df68
Adhammuhmmed <adhammuhmmed22@gmail.com>
2024-08-20 04:14:54 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/freshcart/",
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks(id) {
-  //         if (id.includes('node_modules')) {
-  //           return 'vendor'; 
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; 
+          }
+        }
+      }
+    }
+  }
 },
 
 
